b8ea77c98d3f99c4a951dec7c9ed20ec195a066a
scalepro <113922133+scalepro@users.noreply.github.com>
2022-10-04 01:02:55 -0300

    Update Address.tsx

--- src/components/assinatura/Address.tsx
+++ src/components/assinatura/Address.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Controller } from 'react-hook-form';
 import MaskedInput from 'react-text-mask';
@@ -7,7 +7,12 @@ import { classNames } from '@/services/functions';
 import LoadingSpin from '@/components/app/LoadingSpin';
 import toast, { Toaster } from 'react-hot-toast';
 import Toast from '@/components/app/Toast';
-import { HiXCircle } from 'react-icons/hi';
+import {
+  defaultInput,
+  errorInput,
+  defaultLabel,
+  errorFormMessage,
+} from '@/styles/StyledElements';
 
 export default function Address({ register, errors, control }) {
   const states = State.getStatesOfCountry('BR');
@@ -16,11 +21,6 @@ export default function Address({ register, errors, control }) {
   const [selectedAddress, setSelectedAddress] = useState('');
   const [selectedNeighborhood, setSelectedNeighborhood] = useState('');
   const [loadingCep, setLoadingCep] = useState(false);
-
-  const errorInputClass =
-    "bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500'";
-  const defaultInputClass =
-    'bg-gray-50 border border-gray-300 text-gray-700 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500';
 
   const cities = (selectedState) => {
     return selectedState ? City.getCitiesOfState('BR', selectedState) : [];
@@ -92,10 +92,7 @@ export default function Address({ register, errors, control }) {
       <Toaster position="top-center" reverseOrder={false} />
       <div className="col-span-4 sm:col-span-2">
         <div className="relative">
-          <label
-            htmlFor="cep"
-            className="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-300"
-          >
+          <label htmlFor="cep" className={defaultLabel}>
             CEP
           </label>
           <Controller
@@ -121,8 +118,8 @@ export default function Address({ register, errors, control }) {
                         errors.cep.type === 'required' ||
                         errors.cep.type === 'minLength' ||
                         errors.cep.type === 'maxLength')
-                      ? errorInputClass
-                      : defaultInputClass,
+                      ? errorInput
+                      : defaultInput,
                     'focus-visible:ring-primary-500 focus-visible:border-primary-500'
                   )}
                   placeholder="00000-000"
@@ -134,29 +131,22 @@ export default function Address({ register, errors, control }) {
           {errors.cep &&
             (((errors.cep.type === 'minLength' ||
               errors.cep.type === 'maxLength') && (
-              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
-                Número de CEP incorreto
-              </p>
+              <p className={errorFormMessage}>Número de CEP incorreto</p>
             )) ||
               (errors.cep.type === 'required' && (
-                <p className="mt-2 text-sm text-red-600 dark:text-red-500">
-                  Este campo é obrigatório
-                </p>
+                <p className={errorFormMessage}>Este campo é obrigatório</p>
               )))}
         </div>
       </div>
       <div className="col-span-4 sm:col-span-2">
-        <label
-          htmlFor="state"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-        >
+        <label htmlFor="state" className={defaultLabel}>
           Estado
         </label>
         <select
           id="state"
           name="state"
           className={classNames(
-            errors.state && !selectedState ? errorInputClass : defaultInputClass
+            errors.state && !selectedState ? errorInput : defaultInput
           )}
           {...register('state', { required: true })}
           onChange={(event) => onHandleState(event)}
@@ -164,29 +154,28 @@ export default function Address({ register, errors, control }) {
         >
           <option value="">Selecione um estado</option>
           {states.map((state, stateIdx) => (
-            <option value={state.isoCode} key={stateIdx}>
+            <option
+              suppressHydrationWarning
+              value={state.isoCode}
+              key={stateIdx}
+            >
               {state.name}
             </option>
           ))}
         </select>
         {errors.state && !selectedState && (
-          <p className="mt-2 text-sm text-red-600 dark:text-red-500">
-            Este campo é obrigatório
-          </p>
+          <p className={errorFormMessage}>Este campo é obrigatório</p>
         )}
       </div>
       <div className="col-span-4 sm:col-span-2">
-        <label
-          htmlFor="city"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-        >
+        <label htmlFor="city" className={defaultLabel}>
           Cidade
         </label>
         <select
           id="city"
           name="city"
           className={classNames(
-            errors.city && !selectedCity ? errorInputClass : defaultInputClass
+            errors.city && !selectedCity ? errorInput : defaultInput
           )}
           {...register('city', { required: true })}
           onChange={(event) => onHandleCity(event)}
@@ -194,31 +183,24 @@ export default function Address({ register, errors, control }) {
         >
           <option value="">Selecione uma cidade</option>
           {cities(selectedState ? selectedState : null).map((city, cityIdx) => (
-            <option value={city.id} key={cityIdx}>
+            <option suppressHydrationWarning value={city.id} key={cityIdx}>
               {city.name}
             </option>
           ))}
         </select>
         {errors.city && !selectedCity && (
-          <p className="mt-2 text-sm text-red-600 dark:text-red-500">
-            Este campo é obrigatório
-          </p>
+          <p className={errorFormMessage}>Este campo é obrigatório</p>
         )}
       </div>
       <div className="col-span-4 sm:col-span-2">
-        <label
-          htmlFor="address"
-          className="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-300"
-        >
+        <label htmlFor="address" className={defaultLabel}>
           Endereço
         </label>
         <input
           type="text"
           id="address"
           className={classNames(
-            errors.address && !selectedAddress
-              ? errorInputClass
-              : defaultInputClass
+            errors.address && !selectedAddress ? errorInput : defaultInput
           )}
           placeholder="Rua da Independência"
           {...register('address', { required: true })}
@@ -226,41 +208,29 @@ export default function Address({ register, errors, control }) {
           value={selectedAddress}
         />
         {errors.address && !selectedAddress && (
-          <p className="mt-2 text-sm text-red-600 dark:text-red-500">
-            Este campo é obrigatório
-          </p>
+          <p className={errorFormMessage}>Este campo é obrigatório</p>
         )}
       </div>
 
       <div className="col-span-4 sm:col-span-1">
-        <label
-          htmlFor="number"
-          className="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-300"
-        >
+        <label htmlFor="number" className={defaultLabel}>
           Número
         </label>
         <input
           type="text"
           id="number"
           className={classNames(
-            errors.number && !errors.number.message
-              ? errorInputClass
-              : defaultInputClass
+            errors.number && !errors.number.message ? errorInput : defaultInput
           )}
           placeholder="000"
           {...register('number', { required: true })}
         />
         {errors.number && errors.number.type === 'required' && (
-          <p className="mt-2 text-sm text-red-600 dark:text-red-500">
-            Este campo é obrigatório
-          </p>
+          <p className={errorFormMessage}>Este campo é obrigatório</p>
         )}
       </div>
       <div className="col-span-4 sm:col-span-1">
-        <label
-          htmlFor="complement"
-          className="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-300"
-        >
+        <label htmlFor="complement" className={defaultLabel}>
           Complemento
         </label>
         <input
@@ -268,8 +238,8 @@ export default function Address({ register, errors, control }) {
           id="complement"
           className={classNames(
             errors.complement && !errors.complement.message
-              ? errorInputClass
-              : defaultInputClass
+              ? errorInput
+              : defaultInput
           )}
           placeholder="Apto 000"
           {...register('complement')}
@@ -277,10 +247,7 @@ export default function Address({ register, errors, control }) {
       </div>
 
       <div className="col-span-4 sm:col-span-2">
-        <label
-          htmlFor="neighborhood"
-          className="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-300"
-        >
+        <label htmlFor="neighborhood" className={defaultLabel}>
           Bairro
         </label>
         <input
@@ -288,8 +255,8 @@ export default function Address({ register, errors, control }) {
           id="neighborhood"
           className={classNames(
             errors.neighborhood && !selectedNeighborhood
-              ? errorInputClass
-              : defaultInputClass
+              ? errorInput
+              : defaultInput
           )}
           placeholder="Alphaville"
           {...register('neighborhood', { required: true })}
@@ -297,9 +264,7 @@ export default function Address({ register, errors, control }) {
           value={selectedNeighborhood}
         />
         {errors.neighborhood && !selectedNeighborhood && (
-          <p className="mt-2 text-sm text-red-600 dark:text-red-500">
-            Este campo é obrigatório
-          </p>
+          <p className={errorFormMessage}>Este campo é obrigatório</p>
         )}
       </div>
     </>
